d70f4b613593c3c8ca3beb06e7290e6d5df3333c
jonathan potter <jonathan.potter.business@gmail.com>
2017-03-10 17:53:24 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# basic-webpack-2-setup
+This is an example starting point for personal projects using Webpack 2 with es6, scss, uglification and sourcemaps.
+
+### Instructions
+[go to the Medium post here for step-by-step instructions](https://medium.com/@jonathanpotter_80804/step-by-step-webpack-2-setup-for-personal-projects-f0b74072daa1#.nhhmx5nmc)
+
+### Build as is
+
+- clone project
+- npm install
+- npm start
+- open browser to http://localhost:8080
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: './javascript/index',
+  output: {
+    path: __dirname,
+    filename: 'build/bundle.js',
+    sourceMapFilename: 'sourcemap'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      /* eslint-disable key-spacing */
+      css: path.resolve(__dirname, 'css'),
+      javascript: path.resolve(__dirname, 'javascript')
+      /* eslint-enable key-spacing */
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$|.jsx$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css|.scss$/,
+        use: ExtractTextPlugin.extract({
+          loader: [
+            { loader: 'css-loader', options: { sourceMap: true } },
+            { loader: 'sass-loader', options: { sourceMap: true } }
+          ]
+        })
+      }
+    ]
+  },
+  devtool: '#inline-source-map',
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'build/style.css',
+      allChunks: true
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true
+    })
+  ]
+}
